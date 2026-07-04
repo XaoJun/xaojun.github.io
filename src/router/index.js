0fd534c0dccd7339d72077397b4a32ece4808a2d
tianyanjun <1781499575@qq.com>
2026-07-05 03:59:04 +0800

    完成标签页的信息

--- src/router/index.js
+++ src/router/index.js
@@ -7,33 +7,43 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
+      meta: { title: '田艳军 | 个人博客 - 技术实践与生活片段' },
     },
     {
       path: '/photos',
       name: 'photos',
       component: () => import('../views/PhotosView.vue'),
+      meta: { title: '日常照片 | 田艳军的个人博客' },
     },
     {
       path: '/blog',
       name: 'blog',
       component: () => import('../views/BlogListView.vue'),
+      meta: { title: '技术博客 | 田艳军的个人博客' },
     },
     {
       path: '/blog/:slug',
       name: 'blog-detail',
       component: () => import('../views/BlogDetailView.vue'),
+      meta: { title: '文章详情 | 田艳军的个人博客' },
     },
     {
       path: '/resume',
       name: 'resume',
       component: () => import('../views/ResumeView.vue'),
+      meta: { title: '个人简历 | 田艳军的个人博客' },
     },
     {
       path: '/contact/:type',
       name: 'contact-detail',
       component: () => import('../views/ContactDetailView.vue'),
+      meta: { title: '联系方式 | 田艳军的个人博客' },
     },
   ],
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || '田艳军 | 个人博客'
 })
 
 export default router
